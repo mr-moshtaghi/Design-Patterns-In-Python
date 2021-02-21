65c6c64d83c208fd5673fe8d76bbfe297a151019
mr-moshtaghi <mr.ahmadalimoshtaghi@gmail.com>
2021-02-21 15:52:30 +0330

    strategy

---
+++ 11.00.strategy.py
@@ -0,0 +1,33 @@
+"""
+    Strategy
+"""
+from abc import ABC, abstractmethod
+
+
+class Context:
+    def __init__(self, direction, sentence):
+        self._direction = direction
+        self.sentence = sentence
+
+    def sorting(self):
+        return self._direction.direct(self.sentence)
+
+
+class Direction(ABC):
+    @abstractmethod
+    def direct(self, data):
+        pass
+
+
+class Right(Direction):
+    def direct(self, data):
+        print(data[::-1])
+
+
+class Left(Direction):
+    def direct(self, data):
+        print(data)
+
+
+c1 = Context(Right(), 'Hello World...')
+c1.sorting()
